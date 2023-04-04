2d8bfe5d2145ce874597cc8c5e35faa17b5b618b
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-04-04 14:06:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
